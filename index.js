72c9c443be790d11ee545b31296e5d14925a4a4b
smaykellguerrero <smaykellguerrero@gmail.com>
2019-12-19 14:55:49 -0500

    preview final

--- index.js
+++ index.js
@@ -7,11 +7,23 @@ const app = express()
 
 app
     //.use(logger("dev"))
+    .use((req, res, next) => {
+        res.set({
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            'Access-Control-Allow-Credentials': 'true'
+        })
+        if (req.method === "OPTIONS") {
+            res.set({ "Access-Control-Allow-Headers": "Content-Type" })
+            return res.end()
+        }
+        next()
+    })
+
     .use('/graphql', graphqlHTTP({
         schema: schema,
-        graphiql: true,
+        pretty: true,
+        graphiql: true
     }))
-
 
     .listen(port, function () {
         console.log(`server running on port ${port}`)
